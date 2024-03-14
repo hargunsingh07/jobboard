921c3ede98c34b8898f755670c0461d3fbd331e0
hargunsingh07 <hargunsingh@academic.rrc.ca>
2024-03-14 16:39:16 -0500

    Added search in job controller

--- app/controllers/job_listings_controller.rb
+++ app/controllers/job_listings_controller.rb
@@ -2,6 +2,7 @@ class JobListingsController < ApplicationController
   def index
     if params[:search].present? && params[:company_id].present?
       @job_listings = JobListing.where('title LIKE ? AND company_id = ?', "%#{params[:search]}%", params[:company_id]).page(params[:page]).per(10)
+
     elsif params[:search].present?
       @job_listings = JobListing.where('title LIKE ?', "%#{params[:search]}%").page(params[:page]).per(10)
     elsif params[:company_id].present?
